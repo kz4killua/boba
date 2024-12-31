3a9e240fb27019f7349d498621046fb070cf2aaf
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-12-30 20:40:24 -0500

    Add a reset() method to the parser

--- src/compiler/parser.ts
+++ src/compiler/parser.ts
@@ -3,9 +3,34 @@ import { default as lexer } from "./lexer";
 import { default as grammar } from "./grammar";
 
 
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar), {
-  lexer: lexer,
-});
+class Parser {
+
+  parser: nearley.Parser;
+  results: any[];
+
+  constructor() {
+    this.parser = getParser();
+    this.results = [];
+  }
+
+  reset() {
+    this.parser = getParser();
+    this.results = [];
+  }
+
+  feed(input: string) {
+    this.parser.feed(input);
+    this.results = this.parser.results;
+  }
+
+}
 
 
-export default parser;
+function getParser() {
+  return new nearley.Parser(nearley.Grammar.fromCompiled(grammar), {
+    lexer: lexer,
+  });
+}
+
+
+export default new Parser();
