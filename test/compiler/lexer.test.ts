65a4b2802791c38544717f2b212515e8a0cd9ef7
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-12-30 19:37:40 -0500

    Set up Jest and nearley

---
+++ test/compiler/lexer.test.ts
@@ -0,0 +1,42 @@
+import lexer from "../../src/compiler/lexer";
+
+
+describe('Lexer', () => {
+
+  test('Non-keywords e.g. repeat-ed, or-ange, set-tled', () => {
+
+    let input, tokens, expected;
+    
+    input = 'repeated';
+    lexer.reset(input);
+    tokens = Array.from(lexer.tokenize());
+    expected = ['IDENTIFIER'];
+    expect(tokens.map(t => t.type)).toEqual(expected);
+
+    input = 'orange';
+    lexer.reset(input);
+    tokens = Array.from(lexer.tokenize());
+    expected = ['IDENTIFIER'];
+    expect(tokens.map(t => t.type)).toEqual(expected);
+
+  });
+  
+  test('Assignment statements', () => {
+
+    let input, tokens, expected;
+
+    input = 'set complete to true';
+    lexer.reset(input);
+    tokens = Array.from(lexer.tokenize());
+    expected = ['KEYWORD', 'IDENTIFIER', 'KEYWORD', 'BOOLEAN'];
+    expect(tokens.map(t => t.type)).toEqual(expected);
+
+    input = 'set complete to settled';
+    lexer.reset(input);
+    tokens = Array.from(lexer.tokenize());
+    expected = ['KEYWORD', 'IDENTIFIER', 'KEYWORD', 'IDENTIFIER'];
+    expect(tokens.map(t => t.type)).toEqual(expected);
+
+  });
+
+});
