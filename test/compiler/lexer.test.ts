227a4ce3dcd1c804a71638a8718f846256f5558e
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2024-12-30 20:00:53 -0500

    Update lexer test cases

--- test/compiler/lexer.test.ts
+++ test/compiler/lexer.test.ts
@@ -3,6 +3,24 @@ import lexer from "../../src/compiler/lexer";
 
 describe('Lexer', () => {
 
+  test('Text', () => {
+    
+    let input, tokens, expected;
+
+    input = '"(and or not)"';
+    lexer.reset(input);
+    tokens = Array.from(lexer.tokenize());
+    expected = ['TEXT'];
+    expect(tokens.map(t => t.type)).toEqual(expected);
+
+    input = '"# This is a comment"';
+    lexer.reset(input);
+    tokens = Array.from(lexer.tokenize());
+    expected = ['TEXT'];
+    expect(tokens.map(t => t.type)).toEqual(expected);
+
+  });
+
   test('Non-keywords e.g. repeat-ed, or-ange, set-tled', () => {
 
     let input, tokens, expected;
