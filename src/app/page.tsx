852eeb0643f9fe0961df2cebf914427c7875e2b8
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-01-02 08:18:00 -0500

    Add explorer (sidebar)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+import { Explorer } from "@/components/explorer";
 import { TitleBar } from "@/components/title-bar";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
 
 export default function Page() {
   return (
-    <main className="flex flex-col h-screen divide-y">
+    <main className="flex flex-col h-screen w-screen divide-y">
       <TitleBar />
-      <div className="grow">
-
-      </div>
+      <ResizablePanelGroup direction="horizontal" className="grow">
+        <ResizablePanel defaultSize={25}>
+          <Explorer />
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel defaultSize={75}>
+          
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </main>
   )
 }
---
+++ src/components/explorer/index.tsx
@@ -0,0 +1,39 @@
+import { FilePlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+
+export function Explorer() {
+  return (
+    <div>
+      <div className="p-3 text-muted-foreground flex justify-between items-center">
+        <span className="text-sm">Explorer</span>
+        <NewFileButton />
+      </div>
+    </div>
+  )
+}
+
+
+function NewFileButton() {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant={"ghost"} className="p-2 size-6">
+            <FilePlusIcon />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent side="bottom" className="bg-background text-foreground rounded border">
+          <p>New File...</p>
+        </TooltipContent>
+      </Tooltip>
+      </TooltipProvider>
+  )
+}
