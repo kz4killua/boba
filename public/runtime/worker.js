311b0eb3fd8b069a9a809a9952ad98552e569a40
kz4killua <ifeanyi.kz.obinelo@gmail.com>
2025-01-27 22:41:30 -0500

    Update worker.js

--- public/runtime/worker.js
+++ public/runtime/worker.js
@@ -1,14 +1,20 @@
+// Execute all code within the worker's global scope
+let execute = eval;
+
 self.onmessage = function(event) {
+
   // Override console.log to capture logs
   const logs = [];
   console.log = function (...args) {
     logs.push(args.join(' '));
   }
+
   // Execute the supplied code
   try {
-    const result = eval(event.data.code);
+    const result = execute(event.data.code);
     self.postMessage({ result: result, logs: logs });
   } catch (error) {
     self.postMessage({ error: error.message, logs: logs });
   }
+
 }
